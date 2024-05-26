6ce4c6daf2d53949a0d7a9de09fb713075e199d9
Binwen Wu <2020302131043@whu.edu.cn>
2024-05-26 19:32:45 +0800

    style: refresh

--- src/service/index.ts
+++ src/service/index.ts
@@ -72,7 +72,7 @@ export const queryProfileREADME = cache(async () => {
   return {
     repository: {
       object: {
-        text: "Hi, I'm Binwen Wu, a Developer 🚀 from China.",
+        text: "Hi, I'm Binwen Wu, a Developer 🚀 from China. I have a passion for open source technology and am committed to becoming an excellent independent developer 💻",
       },
     },
   }
