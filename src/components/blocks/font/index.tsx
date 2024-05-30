734feada71cf6eb2ec2aa5d173657e7b3f2ae74b
Binwen Wu <2020302131043@whu.edu.cn>
2024-05-30 19:46:43 +0800

    style: refresh font block

--- src/components/blocks/font/index.tsx
+++ src/components/blocks/font/index.tsx
@@ -13,30 +13,30 @@ export const Font = () => {
       className='group grid grid-cols-3 grid-rows-4 items-center justify-items-center bg-surface-1 text-lg font-semibold text-color-1 lg:grid-cols-5 lg:grid-rows-6'
     >
       <AnimateSpan className='block'>Aa</AnimateSpan>
-      <AnimateSpan className='block'>a</AnimateSpan>
-      <AnimateSpan className='block'>A</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
+      <AnimateSpan className='block'>b</AnimateSpan>
+      <AnimateSpan className='block'>C</AnimateSpan>
+      <AnimateSpan>Dd</AnimateSpan>
+      <AnimateSpan>e</AnimateSpan>
+      <AnimateSpan>f</AnimateSpan>
+      <AnimateSpan>Gg</AnimateSpan>
+      <AnimateSpan>H</AnimateSpan>
+      <AnimateSpan>I</AnimateSpan>
+      <AnimateSpan>j</AnimateSpan>
+      <AnimateSpan>Kk</AnimateSpan>
       <Toggle />
-      <AnimateSpan className='block'>Aa</AnimateSpan>
-      <AnimateSpan className='block'>a</AnimateSpan>
-      <AnimateSpan className='block'>A</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
+      <AnimateSpan className='block'>Ll</AnimateSpan>
+      <AnimateSpan className='block'>m</AnimateSpan>
+      <AnimateSpan className='block'>N</AnimateSpan>
+      <AnimateSpan>Oo</AnimateSpan>
+      <AnimateSpan>p</AnimateSpan>
+      <AnimateSpan>q</AnimateSpan>
+      <AnimateSpan>Rr</AnimateSpan>
+      <AnimateSpan>s</AnimateSpan>
+      <AnimateSpan>T</AnimateSpan>
+      <AnimateSpan>u</AnimateSpan>
+      <AnimateSpan>Uu</AnimateSpan>
+      <AnimateSpan>v</AnimateSpan>
+      <AnimateSpan>W</AnimateSpan>
     </Block>
   )
 }
